e84d6eb71df24386812f9a3905e3a32d2d0aaa8f
rasgrass <maniu611@gmail.com>
2015-06-21 20:30:54 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: rasgrass
+ *
+ * Created on 21 czerwca 2015, 20:19
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+    
+    return 0;  
+}
+
